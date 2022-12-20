39c08d460e6af0e435e7fdc93b5ffdbbbdb50914
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-12-20 11:44:10 -0800

    Hierarchical data structure examples

--- Chapter2/hierarchical-data.scm
+++ Chapter2/hierarchical-data.scm
@@ -53,4 +53,38 @@
 (= (cadr (cadr (cadr l3))) 4)
 
 ;; Ex. 2.25
+(define l225_1 (list 1 3 (list 5 7) 9))
+(= (car (cdr (car (cdr (cdr l225_1))))) 7)
+(= (cadr (cadr (cdr l225_1))) 7)
 
+(define l225_2 (list (list 7)))
+(= (car (car l225_2)) 7)
+(= (caar l225_2) 7)
+
+(define l225_3 (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+(= (cadr (cadr (cadr (cadr (cadr (cadr l225_3)))))) 7)
+
+;; Ex 2.26
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(append x y) ; (1 2 3 4 5 6)
+(cons x y)   ; ((1 2 3) 4 5 6)
+(list x y)   ; ((1 2 3) (4 5 6))
+
+;; Ex 2.28
+(define l228 (list (list 1 2) (list 3 4)))
+
+(define (fringe x)
+  (fringe-impl x '()))
+
+(define (fringe-impl x y)
+  (cond ((null? x) y)
+        ((not (pair? x))
+         (cons x y))
+        ;; (else (fringe-impl (cdr x) (fringe-impl (car x) y)))
+        (else (fringe-impl (car x) (fringe-impl (cdr x) y)))
+        ))
+
+(fringe l228)
+(fringe (list l228 l228))
