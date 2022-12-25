f17f67e05f42a555c428281f336195ca594556aa
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-12-24 17:25:31 -0800

    Remaining exercises from Representing Structures

--- Chapter2/representing-structures.scm
+++ Chapter2/representing-structures.scm
@@ -29,41 +29,62 @@
 
 
 ;; Ex. 2.20
-(define (same-parity x . y)
-  (let ((p? (if (even? x) even? odd?))
-        (z (list x)))
-    (reverse (same-parity-rec p? y z))))
+;; (define (same-parity x . y)
+;;   (let ((p? (if (even? x) even? odd?))
+;;         (z (list x)))
+;;     (reverse (same-parity-rec p? y z))))
 
-(define (same-parity-iter p? y z)
-  (if (null? y)
-      z
-      (if (p? (car y))
-          (same-parity-iter p? (cdr y) (cons (car z) (cons (car y) (cdr z))))
-          ;; (same-parity-iter p? (cdr y) (cons (cons (car z) (car y)) (cdr z)))
-          ;; (same-parity-iter p? (cdr y) (cons (car z) (cons (cdr z) (car y))))
-          (same-parity-iter p? (cdr y) z))))
+;; (define (same-parity-iter p? y z)
+;;   (if (null? y)
+;;       z
+;;       (if (p? (car y))
+;;           (same-parity-iter p? (cdr y) (cons (car z) (cons (car y) (cdr z))))
+;;           ;; (same-parity-iter p? (cdr y) (cons (cons (car z) (car y)) (cdr z)))
+;;           ;; (same-parity-iter p? (cdr y) (cons (car z) (cons (cdr z) (car y))))
+;;           (same-parity-iter p? (cdr y) z))))
 
-(define (same-parity-rec p? y z)
-  (if (null? y)
-      z
-      (if (p? (car y))
-          (same-parity-rec p? (cdr y) (cons (car y) z))
-          ;; (same-parity-rec p? (cdr y) (append (list (car y)) z))
-          ;; (same-parity-rec p? (cdr y) (cons (car z) (car y)))
-          (same-parity-rec p? (cdr y) z))))
-
-(same-parity 1 2 3 4 5 6 7)
-(same-parity 2 3 4 5 6 7)
+;; (define (same-parity-rec p? y z)
+;;   (if (null? y)
+;;       z
+;;       (if (p? (car y))
+;;           (same-parity-rec p? (cdr y) (cons (car y) z))
+;;           ;; (same-parity-rec p? (cdr y) (append (list (car y)) z))
+;;           ;; (same-parity-rec p? (cdr y) (cons (car z) (car y)))
+;;           (same-parity-rec p? (cdr y) z))))
 
 ;; Revised!
-(define (same-parity-2 x . y)
+(define (same-parity x . y)
   (let ((p? (if (even? x) even? odd?)))
-    (cons x (same-parity-impl-2 p? y))))
+    (cons x (same-parity-impl p? y))))
 
-(define (same-parity-impl-2 p? y)
+(define (same-parity-impl p? y)
   (if (null? y)
-      '()
+      ()
       (if (p? (car y))
-          (cons (car y) (same-parity-impl-2 p? (cdr y)))
-          (same-parity-impl-2 p? (cdr y)))))
+          (cons (car y) (same-parity-impl p? (cdr y)))
+          (same-parity-impl p? (cdr y)))))
 
+(equal? (same-parity 1 2 3 4 5 6 7) (list 1 3 5 7))
+(equal? (same-parity 2 3 4 5 6 7) (list 2 4 6))
+
+;; Ex. 2.21
+(define (square-list items)
+  (if (null? items)
+      ()
+      (cons (* (car items) (car items)) (square-list (cdr items)))))
+
+(define (square-list items)
+  (map (lambda (x) (* x x)) items))
+
+;; Ex 2.23
+;; (define (my-for-each proc x)
+;;   (if (null? x)
+;;       true
+;;       ((proc (car x))
+;;        (my-for-each proc (cdr x))
+;;        true)))
+
+(define (my-for-each proc x)
+  (map (lambda (z) (proc z)) x) true)
+
+(my-for-each (lambda (x) (newline) (display x)) (list 57 321 88))
