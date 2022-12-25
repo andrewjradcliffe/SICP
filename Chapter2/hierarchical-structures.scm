6e7cbc111d7761f5661d965772a2df64045f25ca
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-12-24 17:36:41 -0800

    `tree-map`

--- Chapter2/hierarchical-structures.scm
+++ Chapter2/hierarchical-structures.scm
@@ -81,3 +81,39 @@
 (fringe (list l228 l228))
 
 ;; Ex. 2.29
+
+;; Ex. 2.30
+
+(define (square-tree t)
+  (cond ((null? t) ())
+        ((not (pair? t)) (* t t))
+        (else (cons (square-tree (car t)) (square-tree (cdr t))))))
+
+(define (square-tree t)
+  (map (lambda (x)
+         (if (pair? x)
+             (square-tree x)
+             (* x x)))
+       t))
+
+(square-tree (list 1
+                   (list 2 (list 3 4) 5)
+                   (list 6 7)))
+
+;; Ex. 2.31
+
+(define (tree-map proc t)
+  (cond ((null? t) ())
+        ((not (pair? t) (proc t)))
+        (else (cons (tree-map proc (car t)) (tree-map proc (cdr t))))))
+
+;; Alternate in terms of map
+(define (tree-map proc tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (tree-map proc sub-tree)
+             (proc sub-tree)))
+       tree))
+
+(define (square-tree tree) (tree-map (lambda (x) (* x x)) tree))
+(define (scale-tree tree) (tree-map (lambda (x) (* x factor)) tree))
