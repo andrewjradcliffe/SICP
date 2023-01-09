58e1deb82388c42a03fae9c0d158db1e8dc1b076
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-01-08 19:48:55 -0800

    squash! Section 1

--- Chapter3/Section1--Assignment-and-Local-State/the-costs-of-introducing-assignment.scm
+++ Chapter3/Section1--Assignment-and-Local-State/the-costs-of-introducing-assignment.scm
@@ -32,3 +32,25 @@
   (make-joint my-acc 'citizen-kane 'orson-wells))
 
 ((my-acc-2 'orson-wells 'deposit) 0)
+
+
+;; Ex 3.8
+
+(define f
+  (let ((n 0))
+    (lambda (x)
+      (cond ((= x n)
+             (set! n 1)
+             0)
+            ((not (= x n))
+             (set! n 0)
+             1)))))
+;; fresh define
+(f 0) ; 0
+(f 1) ; 0
+;; then re-define
+(f 1) ; 1
+(f 0) ; 0
+
+(+ (f 0) (f 1)) ; 1, suggests that interpreter selects right-to-left
+(+ (f 1) (f 0)) ; 0, suggests that interpreter selects right-to-left
