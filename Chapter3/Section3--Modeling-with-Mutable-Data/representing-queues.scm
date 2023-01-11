daf6b863d75ea935a715665905c8c2d4e4a93f96
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-01-10 16:50:30 -0800

    Queue

--- Chapter3/Section3--Modeling-with-Mutable-Data/representing-queues.scm
+++ Chapter3/Section3--Modeling-with-Mutable-Data/representing-queues.scm
@@ -1,5 +1,67 @@
 ;; 3.3.2 Representing Queues
 
+;; Ex. 3.21
+(define (print-queue queue)
+  (newline)
+  (display (front-ptr queue))
+  (newline))
+
+;; Ex. 3.22
+
+(define (make-queue)
+  (let ((front-ptr '())
+        (rear-ptr '()))
+    (define (set-front-ptr! item) (set! front-ptr item))
+    (define (set-rear-ptr! item) (set! rear-ptr item))
+    (define (empty-queue?) (null? front-ptr))
+    (define (front-queue)
+      (if (empty-queue?)
+          (error "FRONT called with an empty queue" (cons front-ptr rear-ptr))
+          (car front-ptr)))
+    (define (insert-queue! item)
+      (let ((new-pair (cons item '())))
+        (cond ((empty-queue?)
+               (set-front-ptr! new-pair)
+               (set-rear-ptr! new-pair)
+               dispatch)
+              (else
+               (set-cdr! rear-ptr new-pair)
+               (set-rear-ptr! new-pair)
+               dispatch))))
+    (define (delete-queue!)
+      (cond ((empty-queue?)
+             (error "DELETE called with an empty queue" (cons front-ptr rear-ptr)))
+            (else
+             (set-front-ptr! (cdr front-ptr))
+             dispatch)))
+    (define (dispatch m)
+      (cond ((eq? m 'front-ptr) front-ptr)
+            ((eq? m 'rear-ptr) rear-ptr)
+            ((eq? m 'set-front-ptr!) set-front-ptr!)
+            ((eq? m 'set-rear-ptr!) set-rear-ptr!)
+            ((eq? m 'front-queue) front-queue)
+            ((eq? m 'insert-queue!) insert-queue!)
+            ((eq? m 'delete-queue!) delete-queue!)
+            ((eq? m 'empty-queue?) empty-queue?)
+            (else (error "Unknown method -- MAKE-QUEUE"))))
+    dispatch))
+
+;; External methods
+(define (insert-queue! queue item) ((queue 'insert-queue!) item))
+(define (delete-queue! queue) ((queue 'delete-queue!)))
+(define (set-front-ptr-queue! queue item) ((queue 'set-front-ptr!) item))
+(define (set-rear-ptr-queue! queue item) ((queue 'set-rear-ptr!) item))
+(define (empty-queue? queue) ((queue 'empty-queue?)))
+(define (front-queue queue) ((queue 'front-queue)))
+(define (front-ptr-queue queue) (queue 'front-ptr))
+(define (rear-ptr-queue queue) (queue 'rear-ptr))
+
+(define q1 (make-queue))
+(insert-queue! q1 'a)
+(insert-queue! q1 'b)
+(delete-queue! q1)
+(delete-queue! q1)
+
 ;; Ex. 3.23
 
 (define (front-ptr deque) (car deque))
