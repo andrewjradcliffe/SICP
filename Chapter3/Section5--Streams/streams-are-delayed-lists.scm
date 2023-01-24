43dd6e11bbed659fbee8f991306797713faf9619
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-01-23 16:24:50 -0800

    Exercise 3.52 variants

--- Chapter3/Section5--Streams/streams-are-delayed-lists.scm
+++ Chapter3/Section5--Streams/streams-are-delayed-lists.scm
@@ -44,4 +44,55 @@
 
 ;; Several variations, with amusing results based on return value of accum.
 
+;; First, the expected result given memoization of delayed calls and correct definition
+;; of accum.
 
+(define sum 0) ; 0
+
+(define (accum x) ; 0
+  (set! sum (+ x sum))
+  sum)
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20))) ; 1
+
+(define y (stream-filter even? seq)) ; 3, 6
+
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0)) ; 10
+                         seq))
+
+(stream-ref y 7) ; returns 136, and sum is now 136
+
+(display-stream z) ; prints 10,15,45,55,105,120,190,210, and sum is now 210
+
+;; With memo-proc, the stream-map calls occur only once per argument, hence,
+;; each value of (stream-enumerate-interval 1 20) is added to sum only once.
+;; This behavior differs considerably from the behavior without memo-proc.
+;; Incidentally, with memo-proc, the results of the last 3 expressions should be
+;; the same regardless of the number of calls -- without memo-proc they will
+;; change each time. Even the stream-ref and display-stream calls would change each time
+;; without memo-proc.
+
+;; See p. 90 and 91 for the equivalent exercise performed without memo-proc.
+
+
+;; Second, the expected result given memoization of delayed calls and the incorrect
+;; definition of accum.
+
+(define sum 0) ; 0
+
+(define (accum x) ; 0
+  (set! sum (+ x sum))
+  x)
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20))) ; 1
+
+(define y (stream-filter even? seq)) ; 1 + 2 = 3
+
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0)) ; (1 + 2) + 3 + 4 + 5 = 15
+                         seq))
+
+(stream-ref seq 7) ; 15 + 6 + 7 + 8 = 36
+
+(stream-ref y 7) ; 15 + (6 + ... + 16) = 136, returns 16
+
+(display-stream z) ; 136 + 17 + 18 + 19 + 20 = 210, prints 5, 10, 15, 20
