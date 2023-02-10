cd8a22cf68feb42290dbf847661735a8459e2d18
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-02-09 16:24:08 -0800

    A few more failed experiments at pretty-printing

--- Chapter4/Evaluators/the-lazier-evaluator.scm
+++ Chapter4/Evaluators/the-lazier-evaluator.scm
@@ -9,13 +9,17 @@
   (if (null? seq)
       '()
       (list 'cons (car seq)
-            (literal-list->lazy-list (cdr seq)))))
+            (literal-list->lazy-list (cdr seq)))
+      ;; Actually, everything should be recursively quoted
+      ;; (list 'cons (list 'quote (car seq))
+      ;;       (literal-list->lazy-list (cdr seq)))
+      ))
 
 (define (eval exp env)
   (cond ((self-evaluating? exp) exp)
         ((variable? exp) (lookup-variable-value exp env))
         ((quoted? exp)
-         (if (symbol? (text-of-quotation exp))
+         (if (or (symbol? (text-of-quotation exp)) (number? (text-of-quotation exp)))
              (text-of-quotation exp)
              (eval (quoted-list->lazy-list exp) env)))
         ((assignment? exp) (eval-assignment exp env))
@@ -36,6 +40,18 @@
          (error "Unknown expression type -- EVAL" exp))))
 
 
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output
+           (actual-value input the-global-environment)))
+      (announce-output output-prompt)
+      ;; (if (cons-procedure? output)
+      ;;     (user-print (lookup-variable-value 'x (procedure-environment output))))
+      (user-print output)))
+  (driver-loop))
+
+
 (define the-global-environment (setup-environment))
 (driver-loop)
 (define (cons x y)
@@ -44,3 +60,14 @@
   (z (lambda (p q) p)))
 (define (cdr z)
   (z (lambda (p q) q)))
+;; (define unspecific 'Unspecified-return-value)
+(define (for-each proc items)
+  (if (null? items)
+      'done
+      (begin (proc (car items))
+             (for-each proc (cdr items)))))
+
+
+(define x '(1 2 3))
+
+(define z '((1 2) (3 4)))
