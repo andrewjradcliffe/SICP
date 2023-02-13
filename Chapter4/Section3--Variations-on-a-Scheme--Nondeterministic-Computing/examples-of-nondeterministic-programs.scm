411d6351c6122e8ee3c000fdcb2589ae033d56f4
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-02-13 15:57:29 -0800

    Ex. 4.42

--- Chapter4/Section3--Variations-on-a-Scheme--Nondeterministic-Computing/examples-of-nondeterministic-programs.scm
+++ Chapter4/Section3--Variations-on-a-Scheme--Nondeterministic-Computing/examples-of-nondeterministic-programs.scm
@@ -267,3 +267,142 @@ of Ex. 4.40.
   (multiple-dwelling-arbitrary feasible-dwelling-assignment?))
 (define multiple-dwelling-modified
   (multiple-dwelling-arbitrary feasible-dwelling-assignment-modified?))
+
+
+;; Ex. 4.42
+
+#|
+
+Betty
+_____
+Betty : 3
+Kitty : 2
+
+Ethel
+_____
+Ethel : 1
+Joan  : 2
+
+Joan
+____
+Joan  : 3
+Ethel : 5
+
+Kitty
+_____
+Kitty : 2
+Mary  : 4
+
+Mary
+____
+Mary  : 4
+Betty : 1
+
+
+If Mary : 4 is true, then Kitty : 2 is false and Betty : 1 is false.
+Thus, Betty : 3 is true. Thus, Joan : 3 cannot be true. Thus, Ethel : 5 is true.
+Therefore, Ethel : 1 is false and Joan : 2 must be true. Consequently, one
+deduces Kitty : 1.
+
+Result
+______
+Kitty : 1
+Joan  : 2
+Betty : 3
+Mary  : 4
+Ethel : 5
+
+Interestingly, one can invert the order of the statements and draw the inverted conclusion.
+
+|#
+
+
+;; Ex. 4.43
+
+#|
+
+Part 1
+
+Father                Daughter                Yacht
+______                ________                _____
+Moore                 Mary Ann                Lorna
+Downing               *Lorna                  Melissa
+Hall                  *Gabrielle              Rosalind
+Barnacle              Melissa                 Gabrielle
+Parker                *Rosalind               **Mary Ann
+
+** Simple deduction
+* Inference
+|#
+
+(define (fathers-daughters-yachts)
+  (let ((moore (list 'moore 'mary-ann 'lorna))
+        (barnacle (list 'barnacle 'melissa 'gabrielle)))
+    (let ((downing (list 'downing (amb 'lorna 'gabrielle 'rosalind) 'melissa)))
+      (let ((daughters (filter (lambda (x)
+                                 (not (eq? x (cadr downing))))
+                               (list 'gabrielle 'lorna))))
+        (let ((hall (list 'hall (an-element-of daughters) 'rosalind)))
+          (let ((parker (list 'parker (an-element-of (filter (lambda (x)
+                                                               (and (not (eq? x (cadr downing)))
+                                                                    (not (eq? x (cadr hall)))))
+                                                             (list 'lorna 'rosalind)))
+                               'mary-ann)))
+            (require (eq? (cadr parker)
+                          (cond ((eq? 'gabrielle (cadr downing))
+                                 (caddr downing))
+                                ((eq? 'gabrielle (cadr hall))
+                                 (caddr hall)))))
+            (cond ((eq? 'lorna (cadr parker)) 'parker)
+                  ((eq? 'lorna (cadr downing)) 'downing)
+                  ((eq? 'lorna (cadr hall)) 'hall))))))))
+
+
+#|
+
+Part 2
+
+Father                Daughter                Yacht
+______                ________                _____
+Moore                 *Gabrielle              Lorna
+Downing               *Rosalind               Melissa
+Hall                  *Mary Ann               Rosalind
+Barnacle              Melissa                 Gabrielle
+Parker                *Lorna                  **Mary Ann
+
+** Simple deduction
+* Inference
+
+|#
+
+
+(define (fathers-daughters-yachts)
+  (let ((moore (list 'moore (amb 'gabrielle 'mary-ann 'rosalind) 'lorna))
+        (barnacle (list 'barnacle 'melissa 'gabrielle)))
+    (let ((downing (list 'downing (an-element-of (filter (lambda (x)
+                                                           (not (eq? x (cadr moore))))
+                                                         (list 'lorna 'gabrielle 'rosalind)))
+                          'melissa)))
+      (let ((hall (list 'hall (an-element-of (filter (lambda (x)
+                                                       (and (not (eq? x (cadr moore)))
+                                                            (not (eq? x (cadr downing)))))
+                                                     (list 'gabrielle 'mary-ann 'lorna 'rosalind)))
+                         'rosalind)))
+        (let ((parker (list 'parker (an-element-of (filter (lambda (x)
+                                                             (and (not (eq? x (cadr downing)))
+                                                                  (not (eq? x (cadr hall)))
+                                                                  (not (eq? x (cadr parker)))))
+                                                           (list 'lorna 'rosalind)))
+                             'mary-ann)))
+          (require (eq? (cadr parker)
+                        (cond ((eq? 'moore (cadr downing))
+                               (caddr moore))
+                              ((eq? 'gabrielle (cadr downing))
+                               (caddr downing))
+                              ((eq? 'gabrielle (cadr hall))
+                               (caddr hall)))))
+          (cond ((eq? 'moore (cadr downing)) 'moore)
+                ((eq? 'lorna (cadr downing)) 'downing)
+                ((eq? 'lorna (cadr hall)) 'hall)
+                ((eq? 'lorna (cadr parker)) 'parker)))))))
+
