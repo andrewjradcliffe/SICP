64aece2849d206ebe97c0e7992b817616d45d861
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-02-13 13:40:02 -0800

    Rest of 4.3.1

--- Chapter4/Section3--Variations-on-a-Scheme--Nondeterministic-Computing/amb-and-search.scm
+++ Chapter4/Section3--Variations-on-a-Scheme--Nondeterministic-Computing/amb-and-search.scm
@@ -40,3 +40,91 @@ i <= j <= k must be true, thus, we could think of this as selecting a k, then
 
 ;; 4.37
 
+#|
+Ex. 4.35's version searches                i=low,...,high
+                                           j=i,...,high
+                                           k=j,...,high
+
+We can illustrate with high=4
+
+i = 1
+                    k
+    | (1,1) | (1,2) | (1,3) | (1,4) |
+j   |       | (2,2) | (2,3) | (2,4) |
+    |       |       | (3,3) | (3,4) |
+    |       |       |       | (4,4) |
+
+
+i = 2
+                    k
+    |       |       |       |       |
+j   |       | (2,2) | (2,3) | (2,4) |
+    |       |       | (3,3) | (3,4) |
+    |       |       |       | (4,4) |
+
+
+i = 3
+                    k
+    |       |       |       |       |
+j   |       |       |       |       |
+    |       |       | (3,3) | (3,4) |
+    |       |       |       | (4,4) |
+
+
+i = 4
+                    k
+    |       |       |       |       |
+j   |       |       |       |       |
+    |       |       |       |       |
+    |       |       |       | (4,4) |
+
+
+With i = 1, we see the analogy to an upper triangular matrix,
+which has:
+n * (n + 1) / 2                 non-empty elements
+n = high - i + 1                which we can write as n(i) = high - i + 1
+
+Thus, one searches the number of possibilities:
+    ∑ᵢ₌₁ʰⁱᵍʰ n(i) * (n(i) + 1) / 2
+= ∑ᵢ₌₁ʰⁱᵍʰ (high - i + 1) * (high - i + 1 + 1) / 2
+= (1/2) ∑ᵢ₌₁ʰⁱᵍʰ (high - i + 1) * (high - i + 2)
+
+Expanding:
+(high - i + 1) * (high - i + 2) = high^2 - 2i * high + high + i^2 - 3i + 2
+
+Thus, each term is 𝒪(high^2)
+Therefore, a sum of high terms is 𝒪(high^3)
+
+
+
+
+Ex. 4.37's version searches                i=low,...,high
+                                           j=i,...,high
+                                           high^2 >= k^2
+                                           k^2 = i^2 + j^2
+
+It searches the grid:
+                    j
+    | (1,1) | (1,2) | (1,3) | (1,4) |
+i   |       | (2,2) | (2,3) | (2,4) |
+    |       |       | (3,3) | (3,4) |
+    |       |       |       | (4,4) |
+
+Even if we assume that each (i, j) proposal incurs the full cost of both
+require's (n.b. the sqrt is often skipped), then the total number of points is:
+n * (n + 1) / 2                non-empty elements
+n = high
+
+Thus, high * (high + 1) / 2 elements, which is 𝒪(high^2)
+
+
+
+---
+Ben is certainly correct.
+
+If we consider the cost of the individual operations -- mul, add, sqrt --
+then the prefactors on the order of complexity will display differences, but
+it is unlikely that we could find a value of high such taht Ex. 4.35's version
+is faster than Ex. 4.37's version.
+
+|#
