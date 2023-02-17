4fbd4be98e7871341f1a9431b491b7024449a4da
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-02-17 09:23:50 -0800

    The `amb` evaluator

---
+++ Chapter4/Evaluators/the-amb-evaluator.scm
@@ -0,0 +1,4 @@
+;; The amb evaluator, based on the analyzing evaluator which includes
+;; the syntax from 4.1.2, better handling of internal definitions (4.1.6) and letrec.
+
+(load "~/aradclif/scheme-projects/SICP/Chapter4/Evaluators/the-analyzing-evaluator-with-extra-almond.scm")
