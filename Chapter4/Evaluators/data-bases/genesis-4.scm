a32d0f1eda7f90935ca340cadb11bc74f81005a7
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-02-22 08:32:07 -0800

    Rules used by Ex. 4.69

--- Chapter4/Evaluators/data-bases/genesis-4.scm
+++ Chapter4/Evaluators/data-bases/genesis-4.scm
@@ -7,3 +7,13 @@
 (assert! (wife Lamech Ada))
 (assert! (son Ada Jabal))
 (assert! (son Ada Jubal))
+;; from Ex. 4.63, used in Ex. 4.69
+(assert! (rule (son-by-wife ?m ?s)
+               (and (wife ?m ?w)
+                    (son ?w ?s))))
+(assert! (rule (son-of ?m ?s)
+               (or (son ?m ?s)
+                   (son-by-wife ?m ?s))))
+(assert! (rule (grandson ?g ?s)
+               (and (son-of ?g ?f)
+                    (son-of ?f ?s))))
