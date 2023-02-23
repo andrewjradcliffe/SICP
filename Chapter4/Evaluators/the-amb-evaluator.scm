387238f7ff4492ebac07e6c9d8c8f0cadab51a15
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-02-23 10:53:29 -0800

    Rest of the `amb` evaluator

--- Chapter4/Evaluators/the-amb-evaluator.scm
+++ Chapter4/Evaluators/the-amb-evaluator.scm
@@ -1,5 +1,8 @@
 ;; The amb evaluator, based on the analyzing evaluator which includes
 ;; the syntax from 4.1.2, better handling of internal definitions (4.1.6) and letrec.
+#|
+In essence, begin from the most advanced analyzing evaluator.
+|#
 
 (load "~/aradclif/scheme-projects/SICP/Chapter4/Evaluators/the-vanilla-with-extra-almond-evaluator.scm")
 
@@ -44,6 +47,11 @@
     (lambda (env succeed fail)
       (succeed qval fail))))
 
+(define (analyze-variable exp)
+  (lambda (env succeed fail)
+    (succeed (lookup-variable-value exp env)
+             fail)))
+
 ;; Before we analyze the sequence, we must scan out the defines
 (define (analyze-lambda exp)
   (let ((vars (lambda-parameters exp))
@@ -55,3 +63,153 @@
 ;; Must re-define to overwrite definition used by non-analyzing evaluator.
 (define (make-procedure parameters body env)
   (list 'procedure parameters body env))
+
+
+(define (analyze-if exp)
+  (let ((pproc (analyze (if-predicate exp)))
+        (cproc (analyze (if-consequent exp)))
+        (aproc (analyze (if-alternative exp))))
+    (lambda (env succeed fail)
+      (pproc env
+             (lambda (pred-value fail2)
+               (if (true? pred-value)
+                   (cproc env succeed fail2)
+                   (aproc env succeed fail2)))
+             fail))))
+
+(define (analyze-sequence exps)
+  (define (sequentially a b)
+    (lambda (env succeed fail)
+      (a env
+         (lambda (a-value fail2)
+           (b env succeed fail2))
+         fail)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+        first-proc
+        (loop (sequentially first-proc (car rest-procs))
+              (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+        (error "Empty sequence -- ANALYZE"))
+    (loop (car procs) (cdr procs))))
+
+(define (analyze-definition exp)
+  (let ((var (definition-variable exp))
+        (vproc (analyze (definition-value exp))))
+    (lambda (env succeed fail)
+      (vproc env
+             (lambda (val fail2)
+               (define-variable! var val env)
+               (succeed 'ok fail2))
+             fail))))
+
+(define (analyze-assignment exp)
+  (let ((var (assignment-variable))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env succeed fail)
+      (vproc env
+             (lambda (val fail2)
+               (let ((old-value (lookup-variable-value var env)))
+                 (set-variable-value! var val env)
+                 (succeed 'ok
+                          (lambda ()
+                            (set-variable-value! var old-value env)
+                            (fail2)))))
+             fail))))
+
+(define (analyze-application exp)
+  (let ((fproc (analyze (operator exp)))
+        (aprocs (map analyze (operands exp))))
+    (lambda (env succeed fail)
+      (fproc env
+             (lambda (proc fail2)
+               (get-args aprocs
+                         env
+                         (lambda (args fail3)
+                           (execute-application
+                            proc args succeed fail3))
+                         fail2))
+             fail))))
+
+(define (get-args aprocs env succeed fail)
+  (if (null? aprocs)
+      (succeed '() fail)
+      ((car aprocs) env
+       (lambda (arg fail2)
+         (get-args (cdr aprocs)
+                   env
+                   (lambda (args fail3)
+                     (succeed (cons arg args)
+                              fail3))
+                   fail2))
+       fail)))
+
+(define (execute-application proc args succeed fail)
+  (cond ((primitive-procedure? proc)
+         (succeed (apply-primitive-procedure proc args)
+                  fail))
+        ((compound-procedure? proc)
+         ((procedure-body proc)
+          (extend-environment (procedure-parameters proc)
+                              args
+                              (procedure-environment proc))
+          succeed
+          fail))
+        (else
+         (error
+          "Unknown procedure type -- EXECUTE-APPLICATION"
+          proc))))
+
+(define (analyze-amb exp)
+  (let ((cprocs (map analyze (amb-choices exp))))
+    (lambda (env succeed fail)
+      (define (try-next choices)
+        (if (null? choices)
+            (fail)
+            ((car choices) env
+             succeed
+             (lambda ()
+               (try-next (cdr choices))))))
+      (try-next cprocs))))
+
+(define input-prompt ";;; Amb-Eval input:")
+(define output-prompt ";;; Amb-Eval value:")
+
+(define (driver-loop)
+  (define (internal-loop try-again)
+    (prompt-for-input input-prompt)
+    (let ((input (read)))
+      (if (eq? input 'try-again)
+          (try-again)
+          (begin
+            (newline)
+            (display ";;; Starting a new problem ")
+            (ambeval input
+                     the-global-environment
+                     ;; ambeval success
+                     (lambda (val next-alternative)
+                       (announce-output output-prompt)
+                       (user-print val)
+                       (internal-loop next-alternative))
+                     ;; ambeval failure
+                     (lambda ()
+                       (announce-output ";;; There are no more values of")
+                       (user-print input)
+                       (driver-loop)))))))
+  (internal-loop
+   (lambda ()
+     (newline)
+     (display ";;; There is no current problem")
+     (driver-loop))))
+
+;;;;;;;;;;;;;;;; Tests
+(driver-loop)
+(list (amb 1 2 3) (amb 'a 'b))
+(define (require p)
+  (if (not p) (amb)))
+;; For use with (pythagorean-triples).
+;; Notably, the amb version (Ben) of pythagorean-triples is considerably faster
+;; than the stream-based version.
+(define (an-integer-starting-from n)
+  (amb n (an-integer-starting-from (+ n 1))))
