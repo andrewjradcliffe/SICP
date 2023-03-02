d88235893b964257907f4e0c04018cbce38e6474
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-03-01 16:36:25 -0800

    squash! Controllers, etc. for example machines

--- Chapter5/Simulators/example-controllers.scm
+++ Chapter5/Simulators/example-controllers.scm
@@ -39,3 +39,37 @@
     (assign b (reg t))
     (goto (label test-b))
     gcd-done))
+
+
+(define recursive-fibonacci-registers
+  '(continue n val))
+(define recursive-fibonacci-operations
+  (list (list '+ +) (list '< <) (list '- -)))
+
+;; with eliminations from Ex. 5.6 and Ex. 5.11
+(define recursive-factorial-controller-text
+  '((assign continue (label fib-done))
+    fib-loop
+    (test (op <) (reg n) (const 2))
+    (branch (label immediate-answer))
+    (save continue)
+    (assign continue (label afterfib-n-1))
+    (save n)
+    (assign n (op -) (reg n) (const 1))
+    (goto (label fib-loop))
+    afterfib-n-1
+    (restore n)
+    (assign n (op -) (reg n) (const 2))
+    (assign continue (label afterfib-n-2))
+    (save val)
+    (goto (label fib-loop))
+    afterfib-n-2
+    (assign n (reg val))
+    (restore val)
+    (restore continue)
+    (assign val (op +) (reg val) (reg n))
+    (goto (reg continue))
+    immediate-answer
+    (assign val (reg n))
+    (goto (reg continue))
+    fib-done))
