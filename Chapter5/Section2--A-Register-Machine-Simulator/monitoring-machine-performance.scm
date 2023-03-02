421a79af3ad391f7e2e39e660bd5e7302348d9dc
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-03-02 15:51:01 -0800

    Rest of 5.2.4

--- Chapter5/Section2--A-Register-Machine-Simulator/monitoring-machine-performance.scm
+++ Chapter5/Section2--A-Register-Machine-Simulator/monitoring-machine-performance.scm
@@ -53,6 +53,119 @@ n total-pushes maximum-depth
 |#
 
 
+;; Ex. 5.15
+#|
+There are two possibilities:
+1. to each instruction execution procedure, one must add (machine 'increment-instruction-count).
+
+2. more elegant way: modify execute to call increment-instruction-count! after each instruction
+   is executed; I use this approach.
+|#
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '())
+        (instruction-count 0))
+    (let ((the-ops
+           (list (list 'initialize-stack
+                       (lambda () (stack 'initialize)))
+                 (list 'print-instrunction-count
+                       (lambda () (print-instrunction-count)))
+                 (list 'reset-instruction-count
+                       (lambda () (reset-instruction-count!)))))
+          (register-table
+           (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register: " name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (error "Unknown register: " name))))
+      (define (increment-intruction-count!)
+        (set! instruction-count (+ instruction-count 1)))
+      (define (reset-instruction-count!)
+        (set! instruction-count 0))
+      (define (print-intruction-count)
+        (newline)
+        (display list 'instruction-count '= instruction-count))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+                ((instruction-execution-proc (car insts)))
+                (increment-instruction-count!)
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq) (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register) allocate-register)
+              ((eq? message 'get-register) lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops) (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              ((eq? message 'increment-instruction-count)
+               (increment-instruction-count!))
+              ((eq? message 'reset-instruction-count)
+               (reset-instruction-count))
+              ((eq? message 'print-intruction-count)
+               (print-intruction-count))
+              (else (error "Unknown request -- MACHINE" message))))
+      dispatch)))
+
+
+;; Ex. 5.16
+#|
+There is a simple and elegant way to enact this: modify execute
+|#
+;; Internal definition within make-new-machine
+(define (execute)
+  (let ((insts (get-contents pc)))
+    (if (null? insts)
+        'done
+        (begin
+          (if trace
+              (begin (newline)
+                     (display (instruction-text (car insts)))))
+          ((instruction-execution-proc (car insts)))
+          (execute)))))
+;; Add dispatches to make-new-machine
+((eq? message 'trace-on)
+ (set! trace true))
+((eq? message 'trace-off)
+ (set! trace false))
+#|
+In fact, we can be more efficient within execute by checking trace only once.
+Keep the original execute definition, and add a new definition: execute-with-trace
+|#
+(define (execute-with-trace)
+  (let ((insts (get-contents pc)))
+    (if (null? insts)
+        'done
+        (begin
+          (newline)
+          (display (instruction-text (car insts)))
+          ((instruction-execution-proc (car insts)))
+          (execute-with-trace)))))
+;; Then, in dispatch:
+((eq? message 'start)
+ (set-contents! pc the-instruction-sequence)
+ (if trace
+     (execute-with-trace)
+     (execute)))
+
+
 ;; Ex. 5.17
 #|
 Two versions, the first of which is very indirect, inefficient, and not extensible.
