2c84f3be2ee2c385bd49ebd3cf5c961641003704
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-03-09 10:55:35 -0800

    Explicit-Control Evaluator operations

--- Chapter5/EC-Evaluator/machine-primitives-from-4.1.scm
+++ Chapter5/EC-Evaluator/machine-primitives-from-4.1.scm
@@ -1,1 +1,205 @@
 ;; Machine primitives from Section 4.1 necessary for EC-Eval machine
+
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+        ((string? exp) true)
+        ((null? exp)) ;; useful so that the empty list is recognized.
+        (else false)))
+(define (variable? exp) (symbol? exp))
+
+(define (quoted? exp)
+  (tagged-list? exp 'quote))
+(define (text-of-quotation exp) (cadr exp))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+(define (assignment? exp)
+  (tagged-list? exp 'set!))
+(define (assignment-variable exp) (cadr exp))
+(define (assignment-value exp) (caddr exp))
+
+(define (definition? exp)
+  (tagged-list? exp 'define))
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caadr exp)))
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp)     ; formal parameters
+                   (cddr exp))))   ; body
+
+
+(define (lambda? exp) (tagged-list? exp 'lambda))
+(define (lambda-parameters exp) (cadr exp))
+(define (lambda-body exp) (cddr exp))
+
+(define (if? exp) (tagged-list? exp 'if))
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (begin? exp) (tagged-list? exp 'begin))
+(define (begin-actions exp) (cdr exp))
+(define (last-exp? seq) (null? (cdr seq)))
+(define (first-exp seq) (car seq))
+(define (rest-exps seq) (cdr seq))
+
+(define (application? exp) (pair? exp))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+(define (no-operands? ops) (null? ops))
+(define (first-operand ops) (car ops))
+(define (rest-operands ops) (cdr ops))
+
+(define (true? x)
+  (not (eq? x false)))
+
+(define (false? x)
+  (eq? x false))
+
+(define (make-procedure parameters body env)
+  (list 'procedure parameters body env))
+(define (compound-procedure? p)
+  (tagged-list? p 'procedure))
+(define (procedure-parameters p) (cadr p))
+(define (procedure-body p) (caddr p))
+(define (procedure-environment p) (cadddr p))
+
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+          (error "Too many arguments supplied" vars vals)
+          (error "Too few arguments supplied" vars vals))))
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars))
+             (car vals))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable" var)
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars))
+             (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable -- SET!" var)
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (add-binding-to-frame! var val frame))
+            ((eq? var (car vars))
+             (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (scan (frame-variables frame)
+          (frame-values frame))))
+
+(define (setup-environment)
+  (let ((initial-env
+         (extend-environment (primitive-procedure-names)
+                             (primitive-procedure-objects)
+                             the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+(define (primitive-implementation proc) (cadr proc))
+
+(define primitive-procedures
+  (list (list 'car car)
+        (list 'cdr cdr)
+        (list 'cons cons)
+        (list 'null? null?)
+        (list '+ +)
+        (list '- -)
+        (list '* *)
+        (list '/ /)
+        (list '= =)
+        (list '<= <=)
+        (list '>= >=)
+        (list '> >)
+        (list '< <)
+        (list 'list list) ;; kind of sketchy, but need varargs handling first.
+        ;; (list 'not not) ;; this one is a bit sketchy
+        (list 'newline newline)
+        (list 'display display)
+        (list 'runtime runtime)
+        ;; more primitives
+        (list 'number? number?)
+        (list 'symbol? symbol?)
+        (list 'string? string?)
+        ;; (list 'string<? string<?)
+        ;; (list 'string>? string>?)
+        ;; (list 'string=? string=?)
+        ;; (list 'symbol<? symbol<?)
+        ;; (list 'symbol>? symbol>?)
+        ;; (list 'symbol=? symbol=?)
+        (list 'eq? eq?) ;; most reasonable definition
+        (list 'eqv? eqv?) ;; probably ok
+        (list 'equal? equal?) ;; sketchy
+        ;; math primitives
+        (list 'abs abs)
+        (list 'max 'max)
+        (list 'min 'min)
+        (list 'expt expt)
+        (list 'sqrt sqrt)
+        (list 'sin sin)
+        (list 'cos cos)
+        (list 'tan tan)
+        ))
+
+(define (primitive-procedure-names)
+  (map car
+       primitive-procedures))
+
+(define (primitive-procedure-objects)
+  (map (lambda (proc) (list 'primitive (cadr proc)))
+       primitive-procedures))
+
+;; Requires (eval only once)
+;; (define apply-in-underlying-scheme apply)
+(define (apply-primitive-procedure proc args)
+  (apply
+   (primitive-implementation proc) args))
+
+(define (prompt-for-input string)
+  (newline) (newline) (display string) (newline))
+(define (announce-output string)
+  (newline) (display string) (newline))
+
+(define (user-print object)
+  (if (compound-procedure? object)
+      (display (list 'compound-procedure
+                     (procedure-parameters object)
+                     (procedure-body object)
+                     '<procedure-env>))
+      (display object)))
---
+++ Chapter5/EC-Evaluator/machine-primitives-from-5.4.scm
@@ -0,0 +1,14 @@
+;; Machine primitives from Section 5.4 necessary for EC-Eval machine
+
+(load "~/aradclif/scheme-projects/SICP/Chapter5/EC-Evaluator/machine-primitives-from-4.1.scm")
+
+(define (empty-arglist) '())
+(define (adjoin-arg arg arglist)
+  (append arglist (list arg)))
+(define (last-operand? ops)
+  (null? (cdr ops)))
+(define (no-more-exps? seq) (null? seq))
+
+(define the-global-environment (setup-environment))
+(define (get-global-environment)
+  the-global-environment)
