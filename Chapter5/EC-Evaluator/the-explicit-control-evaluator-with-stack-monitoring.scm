87224c66f197a8f10a6fc1ae4e65d80a8d30f25a
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-03-10 08:31:46 -0800

    EC-Eval with stack monitoring

---
+++ Chapter5/EC-Evaluator/the-explicit-control-evaluator-with-stack-monitoring.scm
@@ -0,0 +1,292 @@
+;; 5.4 The Explicit-Control Evaluator
+#|
+With stack monitoringDrawing upon a several sources, hence, a separate directory.
+|#
+
+(load "~/aradclif/scheme-projects/SICP/Chapter5/EC-Evaluator/the-explicit-control-evaluator.scm")
+
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '()))
+    (let ((the-ops
+           (list (list 'initialize-stack
+                       (lambda () (stack 'initialize)))
+                 (list 'print-stack-statistics
+                       (lambda () (stack 'print-statistics)))))
+          (register-table
+           (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register: " name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (error "Unknown register: " name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+                ((instruction-execution-proc (car insts)))
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq) (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register) allocate-register)
+              ((eq? message 'get-register) lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops) (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              (else (error "Unknown request -- MACHINE" message))))
+      dispatch)))
+
+
+(define (make-stack)
+  (let ((s '())
+        (number-pushes 0)
+        (max-depth 0)
+        (current-depth 0))
+    (define (push x)
+      (set! s (cons x s))
+      (set! number-pushes (+ 1 number-pushes))
+      (set! current-depth (+ 1 current-depth))
+      (set! max-depth (max current-depth max-depth)))
+    (define (pop)
+      (if (null? s)
+          (error "Empty stack -- POP")
+          (let ((top (car s)))
+            (set! s (cdr s))
+            (set! current-depth (- current-depth 1))
+            top)))
+    (define (initialize)
+      (set! s '())
+      (set! number-pushes 0)
+      (set! max-depth 0)
+      (set! current-depth 0)
+      'done)
+    (define (print-statistics)
+      (newline)
+      (display (list 'total-pushes '= number-pushes
+                     'maximum-depth '= max-depth)))
+    (define (dispatch message)
+      (cond ((eq? message 'push) push)
+            ((eq? message 'pop) (pop))
+            ((eq? message 'initialize) (initialize))
+            ((eq? message 'print-statistics)
+             (print-statistics))
+            (else
+             (error "Unknown request -- STACK" message))))
+    dispatch))
+
+(define eceval-controller-text
+  '(
+    read-eval-print-loop
+    (perform (op initialize-stack))
+    (perform
+     (op prompt-for-input) (const ";;; EC-Eval input:"))
+    (assign exp (op read))
+    (assign env (op get-global-environment))
+    (assign continue (label print-result))
+    (goto (label eval-dispatch))
+    print-result
+    (perform (op print-stack-statistics))
+    (perform
+     (op announce-output) (const ";;; EC-Eval value:"))
+    (perform (op user-print) (reg val))
+    (goto (label read-eval-print-loop))
+    unknown-expression-type
+    (assign val (const unknown-expression-type-error))
+    (goto (label signal-error))
+    unknown-procedure-type
+    (restore continue)
+    (assign val (const unknown-procedure-type-error))
+    (goto (label signal-error))
+    signal-error
+    (perform (op user-print) (reg val))
+    (goto (label read-eval-print-loop))
+    eval-dispatch
+    (test (op self-evaluating?) (reg exp))
+    (branch (label ev-self-eval))
+    (test (op variable?) (reg exp))
+    (branch (label ev-variable))
+    (test (op quoted?) (reg exp))
+    (branch (label ev-quoted))
+    (test (op assignment?) (reg exp))
+    (branch (label ev-assignment))
+    (test (op definition?) (reg exp))
+    (branch (label ev-definition))
+    (test (op if?) (reg exp))
+    (branch (label ev-if))
+    (test (op lambda?) (reg exp))
+    (branch (label ev-lambda))
+    (test (op begin?) (reg exp))
+    (branch (label ev-begin))
+    (test (op application?) (reg exp))
+    (branch (label ev-application))
+    (goto (label unknown-procedure-type))
+    ev-self-eval
+    (assign val (reg exp))
+    (goto (reg continue))
+    ev-variable
+    (assign val (op lookup-variable-value) (reg exp) (reg env))
+    (goto (reg continue))
+    ev-quoted
+    (assign val (op text-of-quotation) (reg exp))
+    (goto (reg continue))
+    ev-lambda
+    (assign unev (op lambda-parameters) (reg exp))
+    (assign exp (op lambda-body) (reg exp))
+    (assign val (op make-procedure) (reg unev) (reg exp) (reg env))
+    (goto (reg continue))
+    ev-application
+    (save continue)
+    (save env)
+    (assign unev (op operands) (reg exp))
+    (save unev)
+    (assign exp (op operator) (reg exp))
+    (assign continue (label ev-appl-did-operator))
+    (goto (label eval-dispatch))
+    ev-appl-did-operator
+    (restore unev)
+    (restore env)
+    (assign argl (op empty-arglist))
+    (assign proc (reg val))
+    (test (op no-operands?) (reg unev))
+    (branch (label apply-dispatch))
+    (save proc)
+    ev-appl-operand-loop
+    (save argl)
+    (assign exp (op first-operand) (reg unev))
+    (test (op last-operand?) (reg unev))
+    (branch (label ev-appl-last-arg))
+    (save env)
+    (save unev)
+    (assign continue (label ev-appl-accumulate-arg))
+    (goto (label eval-dispatch))
+    ev-appl-accumulate-arg
+    (restore unev)
+    (restore env)
+    (restore argl)
+    (assign argl (op adjoin-arg) (reg val) (reg argl))
+    (assign unev (op rest-operands) (reg unev))
+    (goto (label ev-appl-operand-loop))
+    ev-appl-last-arg
+    (assign continue (label ev-appl-accum-last-arg))
+    (goto (label eval-dispatch))
+    ev-appl-accum-last-arg
+    (restore argl)
+    (assign argl (op adjoin-arg) (reg val) (reg argl))
+    (restore proc)
+    (goto (label apply-dispatch))
+    apply-dispatch
+    (test (op primitive-procedure?) (reg proc))
+    (branch (label primitive-apply))
+    (test (op compound-procedure?) (reg proc))
+    (branch (label compound-apply))
+    (goto (label unknown-procedure-type))
+    primitive-apply
+    (assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+    (restore continue)
+    (goto (reg continue))
+    compound-apply
+    (assign unev (op procedure-parameters) (reg proc))
+    (assign env (op procedure-environment) (reg proc))
+    (assign env (op extend-environment) (reg unev) (reg argl) (reg env))
+    (assign unev (op procedure-body) (reg proc))
+    (goto (label ev-sequence))
+    ev-begin
+    (assign unev (op begin-actions) (reg exp))
+    (save continue)
+    (goto (label ev-sequence))
+    ev-sequence
+    (assign exp (op first-exp) (reg unev))
+    (test (op last-exp?) (reg unev))
+    (branch (label ev-sequence-last-exp))
+    (save unev)
+    (save env)
+    (assign continue (label ev-sequence-continue))
+    (goto (label eval-dispatch))
+    ev-sequence-continue
+    (restore env)
+    (restore unev)
+    (assign unev (op rest-exps) (reg unev))
+    (goto (label ev-sequence))
+    ev-sequence-last-exp
+    (restore continue)
+    (goto (label eval-dispatch))
+    ev-if
+    (save exp)
+    (save env)
+    (save continue)
+    (assign continue (label ev-if-decide))
+    (assign exp (op if-predicate) (reg exp))
+    (goto (label eval-dispatch))
+    ev-if-decide
+    (restore continue)
+    (restore env)
+    (restore exp)
+    (test (op true?) (reg val))
+    (branch (label ev-if-consequent))
+    ev-if-alternative
+    (assign exp (op if-alternative) (reg exp))
+    (goto (label eval-dispatch))
+    ev-if-consequent
+    (assign exp (op if-consequent) (reg exp))
+    (goto (label eval-dispatch))
+    ev-assignment
+    (assign unev (op assignment-variable) (reg exp))
+    (save unev)
+    (assign exp (op assignment-value) (reg exp))
+    (save env)
+    (save continue)
+    (assign continue (label ev-assignment-1))
+    (goto (label eval-dispatch))
+    ev-assignment-1
+    (restore continue)
+    (restore env)
+    (restore unev)
+    (perform (op set-variable-value!) (reg unev) (reg val) (reg env))
+    (assign val (const ok))
+    (goto (reg continue))
+    ev-definition
+    (assign unev (op definition-variable) (reg exp))
+    (save unev)
+    (assign exp (op definition-value) (reg exp))
+    (save env)
+    (save continue)
+    (assign continue (label ev-definition-1))
+    (goto (label eval-dispatch))
+    ev-definition-1
+    (restore continue)
+    (restore env)
+    (restore unev)
+    (perform (op define-variable!) (reg unev) (reg val) (reg env))
+    (assign val (const ok))
+    (goto (reg continue))
+    ))
+
+
+
+(define eceval
+  (make-machine
+   eceval-registers
+   eceval-operations
+   eceval-controller-text))
+
+;;;;;;;;;;;;;;;; Tests
+(start eceval)
+(define (factorial-rec n)
+  (if (= n 1)
+      1
+      (* (factorial-rec (- n 1)) 1)))
