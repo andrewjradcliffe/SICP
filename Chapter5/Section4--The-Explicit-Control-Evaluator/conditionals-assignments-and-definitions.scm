dc8dbec77f8d7280eef8ed6cf88e01ac9ce11f15
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-03-12 16:45:55 -0700

    Section 5.4.3

--- Chapter5/Section4--The-Explicit-Control-Evaluator/conditionals-assignments-and-definitions.scm
+++ Chapter5/Section4--The-Explicit-Control-Evaluator/conditionals-assignments-and-definitions.scm
@@ -1,1 +1,71 @@
 ;; 5.4.3 Conditionals, Assignments and Definitions
+
+;; Ex. 5.23
+
+;; entry points to be added to eceval-controller-text
+(define ev-cond-text
+  '(ev-cond
+    (assign exp (op cond->if) (reg exp))
+    (goto (label eval-dispatch))))
+(define ev-let-text
+  '(ev-let
+    (assign exp (op let->combination) (reg exp))
+    (goto (label eval-dispatch))))
+(define ev-let*-text
+  '(ev-let*
+    (assign exp (op let*->nested-lets) (reg exp))
+    (goto (label eval-dispatch))))
+(define ev-letrec-text
+  '(ev-letrec
+    (assign exp (op letrec->let) (reg exp))
+    (goto (label eval-dispatch))))
+;; Then, add to eval-dispatch, prior to application?
+(test (op cond?) (reg exp))
+(branch (label ev-cond))
+(test (op let?) (reg exp))
+(branch (label ev-let))
+(test (op let*?) (reg exp))
+(branch (label ev-let*))
+(test (op letrec?) (reg exp))
+(branch (label ev-letrec))
+
+(define orig
+  `(,@ev-cond-text
+    ,@ev-let-text
+    ,@ev-let*-text
+    ,@ev-letrec-text))
+
+;; Ex. 5.24
+(define cond-as-basic-special-form
+  '(ev-cond
+    (assign unev (op cond-clauses) (reg exp))
+    (goto (label ev-cond-clauses))
+    ev-cond-clauses
+    (assign exp (op first-exp) (reg unev))
+    (test (op last-exp?) (reg unev))
+    (branch (label ev-cond-last-clause))
+    (save continue)
+    (save unev)
+    (save env)
+    (save exp)
+    (assign continue (label ev-cond-clause-decide))
+    (assign exp (op cond-predicate) (reg exp))
+    (goto (label eval-dispatch))
+    ev-cond-clause-decide
+    (restore exp)
+    (restore env)
+    (restore unev)
+    (restore continue)
+    (test (op true?) (reg val))
+    (branch (label ev-cond-clause-true))
+    ev-cond-clause-continue
+    (assign unev (op rest-exps) (reg unev))
+    (goto (label ev-cond-clauses))
+    ev-cond-clause-true
+    (assign unev (op cond-action) (reg exp))
+    (save continue)
+    (goto (label ev-sequence))
+    ev-cond-last-clause
+    (assign unev (op cond-action) (reg exp))
+    (save continue)
+    (goto (label ev-sequence))))
