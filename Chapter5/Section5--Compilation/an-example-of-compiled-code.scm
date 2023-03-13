186cbfd61012f8e4e315d5388dd509919d2052a8
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-03-13 16:44:28 -0700

    Ex. 5.33

--- Chapter5/Section5--Compilation/an-example-of-compiled-code.scm
+++ Chapter5/Section5--Compilation/an-example-of-compiled-code.scm
@@ -33,20 +33,47 @@
           'next)))
 (print-compiled-instruction-sequence factorial-alt)
 
-;; Performance tests?
-(define factorial-3
-  (begin (reset-label-counter)
-         (compile
-          '(begin
-             (define (factorial n)
-               (if (= n 1)
-                   1
-                   (* (factorial (- n 1)) n)))
-             (factorial 3))
-          'val
-          'next)))
-(print-compiled-instruction-sequence factorial-3)
+;; Differences
+#|
+factorial-alt requires a save/restore of the env during evaluation of the
+operands, as (factorial (- n 1)) will create an extended environment, but n must
+be available to be looked up (i.e. in the environment in which * is called).
 
+This does not result in any additional save/restores, as the argl does not
+need to be save/restore'd in factorial-alt -- once we have computed (factorial (- n 1)),
+we go directly to a variable lookup, which can simply be cons'd onto the existing argl.
+|#
+
+;; Efficiency of execution
+(load "~/aradclif/scheme-projects/SICP/Chapter5/Compiler/compiled-code-evaluator.scm")
+#|
+As demonstrated below, there is no difference in efficiency in terms of
+maximum depth and number of pushes.
+
+factorial
+_________
+
+n                maximum depth                number of pushes
+1                2                            2
+2                5                            8
+3                8                            14
+4                11                           20
+5                14                           26
+6                17                           32
+
+
+factorial-alt
+_____________
+
+n                maximum depth                number of pushes
+1                2                            2
+2                5                            8
+3                8                            14
+4                11                           20
+5                14                           26
+6                17                           32
+
+|#
 (define (compiled-factorial-eval-with-monitoring n)
   (define compiled-machine
     (make-machine
@@ -76,4 +103,47 @@
   (get-register-contents compiled-machine 'val)
   )
 
+(compiled-factorial-eval-with-monitoring 1)
+(compiled-factorial-eval-with-monitoring 2)
+(compiled-factorial-eval-with-monitoring 3)
+(compiled-factorial-eval-with-monitoring 4)
+(compiled-factorial-eval-with-monitoring 5)
 (compiled-factorial-eval-with-monitoring 6)
+
+
+(define (compiled-factorial-alt-eval-with-monitoring n)
+  (define compiled-machine
+    (make-machine
+     all-regs
+     compiled-code-operations
+     `(
+       (perform (op initialize-stack))
+       ,@(statements
+          (begin (reset-label-counter)
+                 (compile
+                  `(begin
+                     (define (factorial-alt n)
+                       (if (= n 1)
+                           1
+                           (* n (factorial-alt (- n 1)))))
+                     (factorial-alt ,n))
+                  'val
+                  'next)
+                 ))
+       (perform (op print-stack-statistics))
+       )
+     ))
+  (define the-global-environment (setup-environment))
+  (define (get-global-environment) the-global-environment)
+  (set-register-contents! compiled-machine 'env (get-global-environment))
+  (start compiled-machine)
+  (get-register-contents compiled-machine 'val)
+  )
+
+(compiled-factorial-alt-eval-with-monitoring 1)
+(compiled-factorial-alt-eval-with-monitoring 2)
+(compiled-factorial-alt-eval-with-monitoring 3)
+(compiled-factorial-alt-eval-with-monitoring 4)
+(compiled-factorial-alt-eval-with-monitoring 5)
+(compiled-factorial-alt-eval-with-monitoring 6)
+
