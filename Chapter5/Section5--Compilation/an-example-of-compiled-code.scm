9f05073d04e589ae859e330f40ad035562ace627
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-03-13 14:58:54 -0700

    Performance tester

--- Chapter5/Section5--Compilation/an-example-of-compiled-code.scm
+++ Chapter5/Section5--Compilation/an-example-of-compiled-code.scm
@@ -28,4 +28,52 @@
           '(define (factorial-alt n)
              (if (= n 1)
                  1
-                 (* n (factorial-alt (- n 1))))))))
+                 (* n (factorial-alt (- n 1)))))
+          'val
+          'next)))
+(print-compiled-instruction-sequence factorial-alt)
+
+;; Performance tests?
+(define factorial-3
+  (begin (reset-label-counter)
+         (compile
+          '(begin
+             (define (factorial n)
+               (if (= n 1)
+                   1
+                   (* (factorial (- n 1)) n)))
+             (factorial 3))
+          'val
+          'next)))
+(print-compiled-instruction-sequence factorial-3)
+
+(define (compiled-factorial-eval-with-monitoring n)
+  (define compiled-machine
+    (make-machine
+     all-regs
+     compiled-code-operations
+     `(
+       (perform (op initialize-stack))
+       ,@(statements
+          (begin (reset-label-counter)
+                 (compile
+                  `(begin
+                     (define (factorial n)
+                       (if (= n 1)
+                           1
+                           (* (factorial (- n 1)) n)))
+                     (factorial ,n))
+                  'val
+                  'next)
+                 ))
+       (perform (op print-stack-statistics))
+       )
+     ))
+  (define the-global-environment (setup-environment))
+  (define (get-global-environment) the-global-environment)
+  (set-register-contents! compiled-machine 'env (get-global-environment))
+  (start compiled-machine)
+  (get-register-contents compiled-machine 'val)
+  )
+
+(compiled-factorial-eval-with-monitoring 6)
