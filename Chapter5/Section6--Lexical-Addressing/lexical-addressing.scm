4276bb2fd86eef1f5f9ccb7dc31085c8aa5fcc11
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-03-16 13:32:31 -0700

    Ex. 5.40-5.43

--- Chapter5/Section6--Lexical-Addressing/lexical-addressing.scm
+++ Chapter5/Section6--Lexical-Addressing/lexical-addressing.scm
@@ -31,4 +31,138 @@
 
 ;; Ex. 5.40
 (define empty-compile-time-environment '())
+(define (extend-compile-time-environment formals compile-time-env)
+  (cons formals compile-time-env))
+(define (first-compile-time-frame compile-time-env) (car compile-time-env))
+(define (enclosing-compile-time-environment compile-time-env) (cdr compile-time-env))
 
+;; to every procedure that performs compilation, add another argument: compile-time-env
+;; these changes are made in compiler-with-lexical-addressing.scm
+
+(define (compile-lambda-body exp proc-entry compile-time-env)
+  (let ((formals (lambda-parameters exp)))
+    (let ((extended-env
+           (extend-compile-time-environment formals compile-time-env)))
+      (append-instruction-sequences
+       (make-instruction-sequence '(env proc argl) '(env)
+                                  `(,proc-entry
+                                    (assign env (op compiled-procedure-env) (reg proc))
+                                    (assign env
+                                            (op extend-environment)
+                                            (const ,formals)
+                                            (reg argl)
+                                            (reg env))))
+       (compile-sequence (lambda-body exp) 'val 'return extended-env)))))
+
+
+
+;; Ex. 5.41
+(define (find-variable var compile-time-env)
+  (define (env-loop frame-number env)
+    (if (null? env)
+        'not-found
+        (let ((frame (first-compile-time-frame env)))
+          (let ((displacement-number (scan 0 frame)))
+            (if (eq? displacement-number 'not-found)
+                (env-loop (+ frame-number 1)
+                          (enclosing-compile-time-environment env))
+                (make-lexical-address
+                 frame-number
+                 displacement-number))))))
+  (define (scan displacement-number variables)
+    (if (null? variables)
+        'not-found
+        (if (eq? var (car variables))
+            displacement-number
+            (scan (+ displacement-number 1) (cdr variables)))))
+  (env-loop 0 compile-time-env))
+
+
+;; Ex. 5.42
+(define (compile-variable exp target linkage compile-time-env)
+  (let ((lexical-address (find-variable exp compile-time-env)))
+    (if (eq? 'not-found lexical-address)
+        (end-with-linkage
+         linkage
+         (make-instruction-sequence '(env) (list target)
+                                    ;; A daring way to avoid a save/restore of env;
+                                    ;; otherwise: save env, assign the global-env to env
+                                    ;; assign target, restore env.
+                                    ;; Anything stored in target is about to be clobbered
+                                    ;; anyway, thus, it does not matter if we temporarily
+                                    ;; set it to something other than the final value.
+                                    `((assign ,target (op get-global-environment))
+                                      (assign ,target
+                                              (op lookup-variable-value)
+                                              (const ,exp)
+                                              (reg ,target)))))
+        (end-with-linkage
+         linkage
+         (make-instruction-sequence '(env) (list target)
+                                    `((assign ,target
+                                              (op lexical-address-lookup)
+                                              (const ,lexical-address)
+                                              (reg env))))))))
+
+
+(define (compile-assigment exp target linkage compile-time-env)
+  (let ((var (assignment-variable exp))
+        (get-value-code
+         (compile (assignment-value exp) 'val 'next compile-time-env)))
+    (let ((lexical-address (find-variable exp compile-time-env)))
+      (if (eq? 'not-found lexical-address)
+          (end-with-linkage
+           linkage
+           (preserving '(env)
+                       get-value-code
+                       (make-instruction-sequence '(env val) (list target)
+                                                  `((assign ,target (op get-global-environment))
+                                                    (perform (op set-variable-value!)
+                                                             (const ,var)
+                                                             (reg val)
+                                                             (reg ,target))
+                                                    (assign ,target (const ok))))))
+          (end-with-linkage
+           linkage
+           (preserving '(env)
+                       get-value-code
+                       (make-instruction-sequence '(env val) (list target)
+                                                  `((perform op (lexical-address-set!)
+                                                             (const ,lexical-address)
+                                                             (reg val)
+                                                             (reg ,target))
+                                                    (assign ,target (const ok))))))))))
+
+
+;; Ex. 5.43
+#|
+A procedure body is identical to a lambda body, thus, if we perform the syntactic
+transformation on the lambda body, then we have successfully scanned out internal
+definitions -- should should be possible to accomplish by changing the last line
+of compile-lambda-body to be:
+(compile-sequence (internal-definitions-transform (lambda-body exp)) 'val 'return extended-env)
+
+Notably, the scan-out-defines of Ex. 4.16 only transforms to a let statement, which we do not
+quite support yet. Thus, it is worthwhile to implement the transformation directly to lambda.
+|#
+(define (unassigned-list n)
+  (if (= n 0)
+      '()
+      (cons '*unassigned* (unassigned-list (- n 1)))))
+
+(define (internal-definitions-transform body-exps)
+  (let ((defines (filter definition? body-exps)))
+    (if (null? defines)
+        body-exps
+        (let ((regulars (filter (lambda (exp) (not (definition? exp))) body-exps))
+              (variables (map definition-variable defines))
+              (value-exps (map definition-value defines)))
+          (let ((set!-exps (map (lambda (var exp)
+                                  (list 'set! var exp))
+                                variables
+                                value-exps)))
+            (list ;; makes this a body with single expression; necessary for body to be sequence
+             (cons ;; make this an application
+              (make-lambda variables
+                           (append set!-exps regulars))
+              (unassigned-list (length defines)))))))))
