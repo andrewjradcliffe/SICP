6548f9a069d2666d4e22bbafb37e5d988c4e8ed3
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-03-17 11:04:50 -0700

    squash! More test cases

--- Chapter5/Section6--Lexical-Addressing/lexical-addressing.scm
+++ Chapter5/Section6--Lexical-Addressing/lexical-addressing.scm
@@ -323,3 +323,144 @@ compile-lambda to be:
         (display (test-f a b c d e)))))
   (newline)
   (interactive-compiled-test-f-eval-with-monitoring))
+
+;; Test case #3
+(define (test-f-2 a b)
+  (define u (* 2 3 a))
+  (define v (+ 1 2 3 4 5 6 (* a b) (+ a 2)))
+  (define (fact-iter product counter)
+    (if (> counter v)
+        product
+        (fact-iter (* counter product) (+ counter 1))))
+  (+ (fact-iter 1 1) u))
+(define test-f-2-asm
+  (begin (reset-label-counter)
+         (compile
+          '(define (test-f-2 a b)
+            (define u (* 2 3 a))
+            (define v (+ 1 2 3 4 5 6 (* a b) (+ a 2)))
+            (define (fact-iter product counter)
+              (if (> counter v)
+                  product
+                  (fact-iter (* counter product) (+ counter 1))))
+            (+ (fact-iter 1 1) u))
+          'val
+          'next
+          empty-compile-time-environment)))
+(print-compiled-instruction-sequence test-f-2-asm)
+
+
+(define (compiled-test-f-2-eval-with-monitoring a b)
+  (define the-global-environment (setup-environment))
+  (define (get-global-environment) the-global-environment)
+  (define compiled-machine
+    (make-machine
+     all-regs
+     (append (list (list 'get-global-environment get-global-environment))
+             compiled-code-operations)
+     `(
+       (perform (op initialize-stack))
+       ,@(statements
+          (begin (reset-label-counter)
+                 (compile
+                  `(begin
+                     (define (test-f-2 a b)
+                       (define u (* 2 3 a))
+                       (define v (+ 1 2 3 4 5 6 (* a b) (+ a 2)))
+                       (define (fact-iter product counter)
+                         (if (> counter v)
+                             product
+                             (fact-iter (* counter product) (+ counter 1))))
+                       (+ (fact-iter 1 1) u))
+                     (test-f-2 ,a ,b))
+                  'val
+                  'next
+                  empty-compile-time-environment)
+                 ))
+       (perform (op print-stack-statistics))
+       )
+     ))
+  (set-register-contents! compiled-machine 'env (get-global-environment))
+  (start compiled-machine)
+  (get-register-contents compiled-machine 'val)
+  )
+(define (interactive-compiled-test-f-2-eval-with-monitoring)
+  (display ";;; enter (a b)")
+  (newline)
+  (let ((ab (read)))
+    (let ((a (car ab))
+          (b (cadr ab)))
+      (let ((result (compiled-test-f-2-eval-with-monitoring a b)))
+        (newline)
+        (display "test-f-2 of ")
+        (display ab)
+        (display " = ")
+        (display result)
+        (display "    check value of test-f-2 = ")
+        (display (test-f-2 a b)))))
+  (newline)
+  (interactive-compiled-test-f-2-eval-with-monitoring))
+
+;; Test case #4
+(define (test-f-3 a b)
+  (define u 1)
+  (define v 2)
+  (+ u v a b))
+(define test-f-3-asm
+  (begin (reset-label-counter)
+         (compile
+          '(define (test-f-3 a b)
+             (define u 1)
+             (define v 2)
+             (+ u v a b))
+          'val
+          'next
+          empty-compile-time-environment)))
+(print-compiled-instruction-sequence test-f-3-asm)
+
+(define (compiled-test-f-3-eval-with-monitoring a b)
+  (define the-global-environment (setup-environment))
+  (define (get-global-environment) the-global-environment)
+  (define compiled-machine
+    (make-machine
+     all-regs
+     (append (list (list 'get-global-environment get-global-environment))
+             compiled-code-operations)
+     `(
+       (perform (op initialize-stack))
+       ,@(statements
+          (begin (reset-label-counter)
+                 (compile
+                  `(begin
+                     (define (test-f-3 a b)
+                       (define u 1)
+                       (define v 2)
+                       (+ u v a b))
+                     (test-f-3 ,a ,b))
+                  'val
+                  'next
+                  empty-compile-time-environment)
+                 ))
+       (perform (op print-stack-statistics))
+       )
+     ))
+  (set-register-contents! compiled-machine 'env (get-global-environment))
+  (start compiled-machine)
+  (get-register-contents compiled-machine 'val)
+  )
+(define (interactive-compiled-test-f-3-eval-with-monitoring)
+  (display ";;; enter (a b)")
+  (newline)
+  (let ((ab (read)))
+    (let ((a (car ab))
+          (b (cadr ab)))
+      (let ((result (compiled-test-f-3-eval-with-monitoring a b)))
+        (newline)
+        (display "test-f-3 of ")
+        (display ab)
+        (display " = ")
+        (display result)
+        (display "    check value of test-f-3 = ")
+        (display (test-f-3 a b)))))
+  (newline)
+  (interactive-compiled-test-f-3-eval-with-monitoring))
