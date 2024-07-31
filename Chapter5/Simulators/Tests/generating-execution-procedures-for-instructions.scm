a224316f02ae2e474ccd46f6f4fdf7466230de76
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2024-07-31 12:01:38 -0700

    Add miscellaneous

---
+++ Chapter5/Simulators/Tests/generating-execution-procedures-for-instructions.scm
@@ -0,0 +1,6 @@
+;; Tests of exercises in 5.2.3
+
+;; Ex. 5.9
+(load "~/aradclif/scheme-projects/SICP/Chapter5/Simulators/the-vanilla-register-machine.scm")
+(load "~/aradclif/scheme-projects/SICP/Chapter5/Simulators/example-controllers.scm")
+
